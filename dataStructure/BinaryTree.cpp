6311dab5cda048dc3d987fc8ba9f88770ae7b963
leo-hao <leo_hao_23@163.com>
2017-07-16 09:13:45 +0800

    binarytree
    not

---
+++ dataStructure/BinaryTree.cpp
@@ -0,0 +1,19 @@
+//  Created by Leo on 17/7/15.
+//  Copyright © 2017年 Leo. All rights reserved.
+//  Restore binary tree by in traversal sequence 
+#include<iostream>
+using namespace std;
+typedef struct BiTNode{
+    BiTNode *lchild;
+    BiTNode *rchild;
+    int weight;
+}BiTNode,*BiTree;
+
+
+int main()
+{
+    cout << "请输入前序遍历序列：" <<endl;
+    
+    return 0;
+}
+
